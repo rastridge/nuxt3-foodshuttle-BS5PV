41f91b37121a29ed4d40f93338785331882b5d41
rastridge <rfa@me.com>
2022-11-30 16:30:50 -0500

    index page cleaned out

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,4 @@
 <template>
-	<div class="root">
-		<img src="/bbmontreal.jpeg" class="rounded img-fluid" />
-		<img src="/sam.jpeg" class="rounded img-fluid" />
-		<img src="/zach.jpeg" class="rounded img-fluid" />
-		<img src="/evan.jpeg" class="rounded img-fluid" />
-		<img src="/owen.jpeg" class="rounded img-fluid" />
-		<img src="/millie.jpeg" class="rounded img-fluid" />
-		<img src="/luke.jpeg" class="rounded img-fluid" />
-	</div>
+	<div class="root" style="min-height: 600px"></div>
 </template>
 <script></script>
-<style scoped>
-	img {
-		margin-bottom: 1rem;
-	}
-</style>
