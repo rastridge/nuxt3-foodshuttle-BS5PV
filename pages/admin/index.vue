7b6bfc2a5d38f24cf086a738ac5c33c2eba74d1b
rastridge <rfa@me.com>
2022-12-15 11:21:53 -0500

    cleanup

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,8 +1,12 @@
 <template>
-	<div>stuff</div>
+	<div>Admin user name is {{ auth.user.admin_user_name }}</div>
 </template>
 
 <script setup>
+	import { useAuthStore } from '~~/stores/auth'
+	import { useAlertStore } from '@/stores/alert'
+	const auth = useAuthStore()
+	const alert = useAlertStore()
 	definePageMeta({ layout: 'admin' })
 </script>
 
--- stores/auth.js
+++ stores/auth.js
@@ -64,7 +64,7 @@ export const useAuthStore = defineStore('auth', {
 		},
 		loginSuccess(user) {
 			this.status = { loggedIn: true }
-			// this.user = user
+			this.user = user
 		},
 		loginFailure() {
 			this.status = {}
